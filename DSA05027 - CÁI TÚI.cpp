67350941a3fecafbe67566acb431802b11bc0fa8
Duong Quang Hao <hao510duong@gmail.com>
2024-03-21 11:19:43 +0700

    21/3/2024

---
+++ DSA05027 - CÁI TÚI.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define mp make_pair
+#define fi first
+#define se second
+#define pb push_back
+#define sz size()
+#define ll long long
+#define FOR(i, a, b) for (int i = a; i <= b; ++i)
+#define FORD(i, a, b) for (int i = a; i >= b; --i)
+#define F(i, a, b) for (int i = a; i < b; ++i)
+#define FD(i, a, b) for (int i = a; i > b; --i)
+#define faster() ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL); 
+#define vi vector<int>
+#define vll vector<ll>
+#define vb vector<bool>
+#define pii pair<int, int>
+#define PI 3.141592653589793238
+#define endl '\n'
+const ll INF = 4e18;
+
+int main()
+{
+	faster();
+	int t; cin >> t;
+	while(t--)
+	{
+		int n, W; 
+		cin >> n >> W;
+		ll v[n + 5], w[n + 5];
+		FOR(i, 1, n) cin >> w[i];
+		FOR(i, 1, n) cin >> v[i];
+		vll dp(W + 1, -1);
+		dp[0] = 0;
+		FOR(i, 1, n)
+		{
+			FORD(j, W, w[i])
+			{
+				dp[j] = max(dp[j], dp[j - w[i]] + v[i]);
+			}
+		}
+		ll res = -1;
+		FOR(i, 1, W) res = max(res, dp[i]);
+		cout << res << endl;
+	}
+}
+
